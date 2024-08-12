8d152307b373ec4cf5047a7c78b25c82703ea133
fkhan256 <fkhan25@lakeheadu.ca>
2024-08-12 00:19:26 -0400

    initial commit

--- app/src/main/java/com/example/datanotifier/MainActivity.kt
+++ app/src/main/java/com/example/datanotifier/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.datanotifier
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.widget.Button
 import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,5 +25,48 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("MESSAGE_KEY", message)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_about -> {
+                showAboutDialog()
+                true
+            }
+            R.id.action_help -> {
+                showHelpDialog()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showAboutDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("About")
+        builder.setMessage("This is the DataNotifier app created to demonstrate menu functionality in Android.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
+    }
+
+    private fun showHelpDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Help")
+        builder.setMessage("Enter a message and click 'Send' to display the message in another activity. Use the menu options for more information.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+        }
+        builder.show()
     }
 }
